bee4d2fd851f3f24def30e8876d058dac1404da5
HoKhanhDang <121717409+HoKhanhDang@users.noreply.github.com>
2024-08-20 12:00:02 +0700

    Update ClientChat.jsx

--- src/pages/public/ClientChat.jsx
+++ src/pages/public/ClientChat.jsx
@@ -20,16 +20,13 @@ class Client extends Component {
         addResponseMessage("Are you looking for anything in particular?");
 
         let uid = localStorage.getItem("cc-uid");
-        console.log("Retrieved UID from localStorage:", uid); // Debugging statement
 
         // check for uid, if exist then get auth token
         if (uid !== null) {
             this.fetchAuthToken(uid).then(
                 (result) => {
-                    console.log("auth token fetched", result);
                     // SDK login
                     CometChat.login(result).then((user) => {
-                        console.log("Login successfully:", { user });
                         // listen to incoming message and fetch previous messages
                         this.createMessageListener();
                         this.fetchPreviousMessages();
@@ -51,12 +48,10 @@ class Client extends Component {
     };
 
     createMessageListener = () => {
-        console.log("Creating message listener..."); // Debugging statement
         CometChat.addMessageListener(
             CUSTOMER_MESSAGE_LISTENER_KEY,
             new CometChat.MessageListener({
                 onTextMessageReceived: (message) => {
-                    console.log("Incoming Message Log", { message });
                     addResponseMessage(message.text);
                 },
             })
@@ -70,10 +65,8 @@ class Client extends Component {
             .build();
         messagesRequest.fetchPrevious().then(
             (messages) => {
-                console.log("Message list fetched:", messages);
                 // add messages to the widget chat bubbles
                 messages.forEach((message) => {
-                    console.log("Message Log", { message });
                     if (message.rawMessage.sender === agentUID) {
                         addResponseMessage(message.text);
                     } else {
@@ -104,8 +97,6 @@ class Client extends Component {
     };
 
     handleNewUserMessage = (newMessage) => {
-        console.log(`New message incoming! ${newMessage}`);
-
         const textMessage = new CometChat.TextMessage(
             agentUID,
             newMessage,
@@ -118,13 +109,10 @@ class Client extends Component {
             // no uid, create user
             this.createUser().then(
                 (result) => {
-                    console.log("auth token fetched", result);
                     localStorage.setItem("cc-uid", result.uid);
-                    console.log("Stored UID in localStorage:", result.uid); // Debugging statement
 
                     // do login
                     CometChat.login(result.authToken).then((user) => {
-                        console.log("Login successfully:", { user });
                         CometChat.sendMessage(textMessage).then(
                             (message) => {
                                 console.log(
